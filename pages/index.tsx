b547f739ae534b65950b1c5b119cf2c39d54a167
ALMAS ASSET <assetalmas07@gmail.com>
2023-10-11 00:18:51 +0600

    1.6v Image replaced

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,7 +64,7 @@ export default function Home() {
 <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <a href="" className="flex items-center">
-      <img src="/nis.png" className="h-8 mr-3" alt="Flowbite Logo"></img>
+      <Image src="/nis.png" className="h-8 mr-3" alt="Flowbite Logo" width={30} height={90}/>
       <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">FastTyping Tournament</span>
   </a>
 
